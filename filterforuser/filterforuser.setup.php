35a12c7fb127b56ae87ffc56d8d2fcbdef2746b8
Yaroslav Romanenko <yaroslav.romanenko@gmail.com>
2015-03-25 21:43:01 +0200

    Перша версія

---
+++ filterforuser/filterforuser.setup.php
@@ -0,0 +1,22 @@
+<?php defined('COT_CODE') or die('Wrong URL');
+/* ====================
+[BEGIN_COT_EXT]
+Code=filterforuser
+Name=Addition filter
+Description=Additional filter for users (extrafield)
+Version=1.0.0
+Date=2015-10-03
+Author=CrazyFreeMan
+Copyright=(c) CrazyFreeMan (simple-website.in.ua), 2015
+Notes=
+Auth_guests=R
+Lock_guests=2345A
+Auth_members=RW
+Lock_members=2345
+Requires_modules=users
+[END_COT_EXT]
+
+[BEGIN_COT_EXT_CONFIG]
+fu_usetitle_for_first=01:radio:0,1:1
+[END_COT_EXT_CONFIG]
+==================== */
